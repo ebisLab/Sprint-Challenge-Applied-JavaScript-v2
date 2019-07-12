595fad7102def4d70efea2c01bd0bb68346efc2c
ebisLab <ebaiden@techie.com>
2019-07-12 12:01:18 -0400

    header component added

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,38 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+// chino = document.querySelector('.date').innerText='why';
+// console.log(chino)
+
+
+
+
+
+headerContainer.appendChild(Header())
+
+
+function Header() {
+
+    const header= document.createElement('div');
+    const span = document.createElement('span');
+    const h1= document.createElement('h1');
+    const tempspan= document.createElement('span');
+
+
+
+    header.appendChild(span);
+    header.appendChild(h1);
+    header.appendChild(tempspan);
+
+    header.classList.add('header')
+    span.classList.add('date');
+    tempspan.classList.add('temp');
+
+    span.textContent = 'Some Date';
+    h1.textContent = 'Lambda Times';
+    tempspan.textContent = '98'
+
+    
+    return header;
+}
